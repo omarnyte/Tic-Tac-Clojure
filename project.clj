be598496b3a159451e1033dd382bf2215914c81d
Omar De Los Santos <omarnyte@gmail.com>
2018-05-24 14:34:54 -0700

    Update description and url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tic-tac-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple Tic Tac Toe game written in Clojure."
+  :url "https://github.com/omarnyte/Tic-Tac-Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
