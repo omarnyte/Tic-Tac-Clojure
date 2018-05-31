5dbc6c71fc119650b46e097f8f7e6bfd952b749d
Omar De Los Santos <omarnyte@gmail.com>
2018-05-31 15:01:05 -0700

    Fix typos in valid-move? method

---
+++ src/tic_tac_clojure/gameplay.clj
@@ -0,0 +1,35 @@
+(ns tic-tac-clojure.gameplay
+  (:gen-class)
+  (:require [tic-tac-clojure.board :refer :all])
+  (:require [tic-tac-clojure.game-logic :refer :all])
+  (:require [tic-tac-clojure.render :refer :all]))
+
+(def invalid-move-message
+  (str "That move isn't valid. Please select another move: "))
+
+(def selection-prompt
+  (str "Please choose an index to mark your move: "))
+
+(def game-over-message
+  (str "Game over!"))
+
+(defn valid-move? 
+  [board idx]
+  (and (number? idx)
+        (< idx 8)
+        (>= idx 0)
+        (empty-space? board idx)))
+
+; (defn make-move
+;   [board idx mark]
+;   (if (empty-space? board idx)
+;     (do 
+;       (render-board (mark-board board idx mark)))
+;     (print invalid-move-message)  
+;   ))
+
+; (defn start-game
+;   ([] (start-game (generate-empty-board)))
+;   ([board] (if (game-over? board)
+;               (str game-over-message)
+;               (str "Game isn't over!"))))
