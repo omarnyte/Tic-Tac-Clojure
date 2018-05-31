58d1358e0be4fb408c5c2ade5598488afed1a2a6
Omar De Los Santos <omarnyte@gmail.com>
2018-05-31 11:24:23 -0700

    Fix test inputs and add 'with-out-str to render-board-test

--- test/tic_tac_clojure/render_test.clj
+++ test/tic_tac_clojure/render_test.clj
@@ -4,15 +4,19 @@
             [tic-tac-clojure.render :refer :all]))
 
 (deftest convert-nils-to-spaces-test
-  (is (= [" " " " " "] (convert-nils-to-spaces [nil nil nil])))) 
+  (is (= [" " " " " "] (convert-nils-to-spaces [nil nil nil])))
+  (is (= ["X" " " "X"] (convert-nils-to-spaces ["X" nil "X"])))) 
  
 (deftest fill-row-test
-  (is (= "   |   |   \n" (fill-row [nil nil nil])))
+  (is (= "   |   |   \n" (fill-row [" " " " " "])))
   (is (= " X | X | O \n" (fill-row ["X" "X" "O"])))
-  (is (= " X |   | O \n" (fill-row ["X" nil "O"])))) 
+  (is (= " X |   | O \n" (fill-row ["X" " " "O"])))) 
 
 (deftest render-board-test 
-  (is (= rendered-empty-board (render-board empty-board)))
-  (is (= rendered-x-victory-board (render-board x-victory-board)))
-  (is (= rendered-o-victory-board (render-board o-victory-board)))
-  (is (= rendered-tied-board (render-board tied-board))))
+  (is (= rendered-empty-board (with-out-str (render-board empty-board))))
+  (is (= rendered-x-victory-board (with-out-str (render-board x-victory-board))))
+  (is (= rendered-o-victory-board (with-out-str (render-board o-victory-board))))
+  (is (= rendered-tied-board (with-out-str (render-board tied-board)))))
+
+
+  
