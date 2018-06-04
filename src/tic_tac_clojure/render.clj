9754034944cc0fb4daa1c672508775b777ff910a
Omar De Los Santos <omarnyte@gmail.com>
2018-06-04 11:21:03 -0700

    Create render-row method and tests in render namespace

--- src/tic_tac_clojure/render.clj
+++ src/tic_tac_clojure/render.clj
@@ -7,14 +7,19 @@
 
 (defn convert-nils-to-spaces
   [row-subvec]
-  (into [] (map (fn [mark] (if (nil? mark)
-                              " "
-                              mark))
-                row-subvec)))
+  (into [] 
+        (map (fn [mark] (if (nil? mark)
+                                  " "
+                                  mark))
+            row-subvec)))
   
 (defn fill-row
   [row-subvec]
   (str " " (string/join " | " row-subvec) " \n"))
+
+(defn render-row
+  [row]
+  (str (fill-row (convert-nils-to-spaces row))))
 
 (defn render-board
   [board]
@@ -24,5 +29,20 @@
     (fill-row (convert-nils-to-spaces (subvec board 3 6)))
     horizontal-divider
     (fill-row (convert-nils-to-spaces (subvec board 6 9)))))
+
+(defn divide-board-into-rows
+  [board] 
+  (partition 3 board))
+    
+; is there a way to avoid passing board into the recursive call (since it isn't used after the initial call)?
+(defn render-board-recur
+  ([board] 
+    (render-board-recur board (divide-board-into-rows board)))
+  ([board rows]
+    (if (> 1 (count rows))
+      (do (print (render-row (first rows)))
+          (print horizontal-divider)
+          (recur board (rest rows)))
+      (print render-row (first rows)))))
 
 
--- test/tic_tac_clojure/render_test.clj
+++ test/tic_tac_clojure/render_test.clj
@@ -12,11 +12,26 @@
   (is (= " X | X | O \n" (fill-row ["X" "X" "O"])))
   (is (= " X |   | O \n" (fill-row ["X" " " "O"])))) 
 
+(deftest render-row-test
+  (is (= "   |   |   \n" (render-row [nil nil nil])))
+  (is (= " X |   | O \n" (render-row ["X" nil "O"]))))
+
+(deftest divide-board-into-rows-test
+  (is (= divided-empty-board (divide-board-into-rows empty-board)))
+  (is (= divided-one-mark-board (divide-board-into-rows one-mark-board)))
+  (is (= divided-tied-board (divide-board-into-rows tied-board))))
+
 ; (deftest render-board-test 
 ;   (is (= rendered-empty-board (with-out-str (render-board empty-board))))
 ;   (is (= rendered-x-victory-board (with-out-str (render-board x-victory-board))))
 ;   (is (= rendered-o-victory-board (with-out-str (render-board o-victory-board))))
 ;   (is (= rendered-tied-board (with-out-str (render-board tied-board)))))
 
+; (deftest render-board-recur-test 
+;   (is (= rendered-empty-board (with-out-str (render-board-recur empty-board))))
+;   (is (= rendered-x-victory-board (with-out-str (render-board-recur x-victory-board))))
+;   (is (= rendered-o-victory-board (with-out-str (render-board-recur o-victory-board))))
+;   (is (= rendered-tied-board (with-out-str (render-board-recur tied-board)))))
 
-  
+  [[nil nil nil] [nil nil nil] [nil nil nil]] 
+  [nil nil nil [nil nil nil] [nil nil nil]]
