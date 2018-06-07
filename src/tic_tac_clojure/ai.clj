93c8daabc7939f492b7f7818b99a02fdf608e3f5
Omar De Los Santos <omarnyte@gmail.com>
2018-06-07 10:28:44 -0700

    Create choose-random-empty-space method to temporarily test game loop with AI

---
+++ src/tic_tac_clojure/ai.clj
@@ -0,0 +1,10 @@
+(ns tic-tac-clojure.ai
+  (:gen-class)
+  (:require [tic-tac-clojure.board :refer :all]))
+
+(defn choose-random-empty-space
+  [board]
+  (let [idx (rand-int 9)]
+  (if (empty-space? board idx)
+      idx
+      (recur board))))
---
+++ test/tic_tac_clojure/ai_test.clj
@@ -0,0 +1,3 @@
+(ns tic-tac-clojure.board-render-test
+  (:require [clojure.test :refer :all]
+            [tic-tac-clojure.ai :refer :all]))
