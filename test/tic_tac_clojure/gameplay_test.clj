66c17134a1f459bb2dfeed6f181861b29c2dae32
Omar De Los Santos <omarnyte@gmail.com>
2018-06-12 16:01:04 -0700

    Remove #valid-game-selection-test

--- test/tic_tac_clojure/gameplay_test.clj
+++ test/tic_tac_clojure/gameplay_test.clj
@@ -2,20 +2,3 @@
   (:require [clojure.test :refer :all]
             [tic-tac-clojure.gameplay :refer :all]
             [tic-tac-clojure.sample-boards :refer :all]))
-
-(def human-x-player
-  {:mark "X" 
-    :human? true})
-
-(def human-o-player
-  {:mark "O" 
-    :human? true})
-
-(def ai-o-player
-  {:mark "O" 
-    :human? false})
-
-(deftest valid-game-selection-test
-  (is (= true (valid-game-selection? 1)))
-  (is (= true (valid-game-selection? 2)))
-  (is (= false (valid-game-selection? 3))))
