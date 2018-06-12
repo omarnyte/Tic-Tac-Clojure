24dedab1e3d65bb453398c5aee0d05a146bccc53
Omar De Los Santos <omarnyte@gmail.com>
2018-06-12 11:19:24 -0700

    Create #choose-best-space and #score-move

--- src/tic_tac_clojure/ai.clj
+++ src/tic_tac_clojure/ai.clj
@@ -3,11 +3,11 @@
   (:require [tic-tac-clojure.board :refer :all]
             [tic-tac-clojure.game-logic :refer :all]))
 
+(declare choose-best-space)
+            
 (defn get-opp-marker
   [marker]
-  (if (= "X" marker)
-      "O"
-      "X"))
+  (if (= "X" marker) "O" "X"))
 
 (defn evaluate-result
   [board ai-marker]
@@ -16,9 +16,31 @@
       (= winner ai-marker) 100 
       (nil? winner) 0
       :else -100)))
-    
-(defn pick-max-score-idx
-  [scores-map]
-  (key (first (sort-by val > scores-map))))
       
-;
+(defn score-move 
+  [board marker score]
+  (if (game-over? board)
+      (evaluate-result board marker)
+      (let [opp-marker (get-opp-marker marker)
+            sim-board (mark-board board (choose-best-space board opp-marker) opp-marker)]
+        (recur sim-board 
+               opp-marker 
+               (* -100 score)))))
+            
+(defn create-idx-scores-map 
+  [board empty-indices marker]
+  (zipmap empty-indices 
+          (map #(score-move (mark-board board % marker) 
+                            marker 
+                            100)
+                empty-indices)))
+
+(defn pick-max-score-idx
+  [idx-scores-map]
+  (key (first (sort-by val > idx-scores-map))))
+
+(defn choose-best-space 
+  [board marker]
+  (let [avail-indices (empty-space-indices board)
+        idx-scores-map (create-idx-scores-map board avail-indices marker)]
+    (pick-max-score-idx idx-scores-map)))
