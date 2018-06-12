783f722e3e3e6d330e1eabd67b41b42b323075d4
Omar De Los Santos <omarnyte@gmail.com>
2018-06-12 10:45:45 -0700

    Create #get-opp-marker and get-opp-marker-test

--- src/tic_tac_clojure/ai.clj
+++ src/tic_tac_clojure/ai.clj
@@ -1,14 +1,19 @@
 (ns tic-tac-clojure.ai
   (:gen-class)
-  (:require [tic-tac-clojure.board :refer :all]))
+  (:require [tic-tac-clojure.board :refer :all]
+            [tic-tac-clojure.game-logic :refer :all]))
 
-(defn choose-random-empty-space
-  [board]
-  (let [idx (rand-int 9)]
-  (if (empty-space? board idx)
-      idx
-      (recur board))))
+(defn get-opp-marker
+  [marker]
+  (if (= "X" marker)
+      "O"
+      "X"))
 
-(defn choose-best-space
-  [board]
-  (choose-random-empty-space board))
+(defn evaluate-result
+  [board ai-marker]
+  (let [winner (winner? board)]
+    (cond
+      (= winner ai-marker) 100 
+      (nil? winner) 0
+      :else -100)))
+    
--- test/tic_tac_clojure/ai_test.clj
+++ test/tic_tac_clojure/ai_test.clj
@@ -2,8 +2,25 @@
   (:require [clojure.test :refer :all]
             [tic-tac-clojure.ai :refer :all]
             [tic-tac-clojure.sample-boards :refer :all]))
+            
+(def sample-scores-map
+  {2 0
+   3 -1 
+   4 -1
+   7 1})
 
-(deftest choose-best-space-test
-  (is (= 4 (choose-best-space minimax-board-1)))
-  (is (= 7 (choose-best-space minimax-board-2)))
-  (is (= 6 (choose-best-space minimax-board-3))))
+(deftest get-opp-marker-test
+  (is (= "O" (get-opp-marker "X")))
+  (is (= "X" (get-opp-marker "O"))))
+   
+(deftest evaluate-result-test
+  (is (= -100 (evaluate-result x-victory-board "O")))
+  (is (= 0 (evaluate-result tied-board "O")))
+  (is (= 100 (evaluate-result o-victory-board "O"))))
+            
+; (deftest pick-max-score-index-test
+;   (is (= 3 (pick-max-score-index sample-scores-map))))
+            
+; (deftest choose-best-space-test
+;   (is (= 7 (choose-best-space minimax-board-2)))
+;   (is (= 6 (choose-best-space minimax-board-3))))
