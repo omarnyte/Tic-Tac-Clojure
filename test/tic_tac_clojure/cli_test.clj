bdd329f481e9ecea3b97e07abb13172740b64aa3
Omar De Los Santos <omarnyte@gmail.com>
2018-06-12 20:07:49 -0700

    Move tests to correct namespaces

--- test/tic_tac_clojure/cli_test.clj
+++ test/tic_tac_clojure/cli_test.clj
@@ -3,13 +3,6 @@
             [tic-tac-clojure.cli :refer :all]
             [tic-tac-clojure.message-render :refer :all]))
 
-(deftest convert-to-num-test
-  (is (= 0 (convert-to-num "0"))))
-
-(deftest is-number-test
-  (is (= true (is-number? "0")))
-  (is (= false (is-number? "a"))))
-
 (deftest print-to-cli-test
   (is (= "Hello, world!\n" (with-out-str (print-to-cli "Hello, world!")))))
 
--- test/tic_tac_clojure/validators_test.clj
+++ test/tic_tac_clojure/validators_test.clj
@@ -1,7 +1,11 @@
 (ns tic-tac-clojure.validators-test
   (:require [clojure.test :refer :all]
-            [tic-tac-clojure.validators :refer :all])
+            [tic-tac-clojure.validators :refer :all]))
 
+(deftest is-number-test
+  (is (= true (is-number? "0")))
+  (is (= false (is-number? "a"))))
+            
 (deftest in-range-test
   (is (= true (in-range? 0 8 0)))
   (is (= true (in-range? 0 8 4)))
