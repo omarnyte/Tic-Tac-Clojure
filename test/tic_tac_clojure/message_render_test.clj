8f0ff13029611a06a187a240a1786af9cdc14f97
Omar De Los Santos <omarnyte@gmail.com>
2018-06-18 15:47:38 -0700

    Write #generate-invalid-move-message-test and #generate-ai-choice-message-test

--- test/tic_tac_clojure/message_render_test.clj
+++ test/tic_tac_clojure/message_render_test.clj
@@ -7,5 +7,13 @@
   (is (= "O wins!" (generate-winner-message "O"))))
 
 (deftest generate-move-selection-prompt-test
-  (is (= "It's your turn, X.\nPlease choose an index to mark your move: " (generate-move-selection-prompt "X")))
-  (is (= "It's your turn, O.\nPlease choose an index to mark your move: " (generate-move-selection-prompt "O"))))
+  (is (= "It's your turn, X.\nPlease choose an index to mark your move: "
+         (generate-move-selection-prompt "X")))
+  (is (= "It's your turn, O.\nPlease choose an index to mark your move: "
+         (generate-move-selection-prompt "O"))))
+
+(deftest generate-invalid-move-message-test
+  (is (= "Please choose a number between 0 and 10." (generate-invalid-range-message 0 10))))
+
+(deftest generate-ai-choice-message-test 
+  (is (= "The computer chose space #0.\n" (generate-ai-choice-message 0))))
