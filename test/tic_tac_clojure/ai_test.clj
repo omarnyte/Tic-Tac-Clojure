5312a1cf439c89b1036ea4520dd440ce3f128ed3
Omar De Los Santos <omarnyte@gmail.com>
2018-06-19 14:34:01 -0700

    Describe tests semantically

--- test/tic_tac_clojure/ai_test.clj
+++ test/tic_tac_clojure/ai_test.clj
@@ -4,5 +4,7 @@
             [tic-tac-clojure.sample-boards :refer :all]))
             
 (deftest choose-best-space-test
-  (is (= 4 (choose-best-space minimax-board-1 "O")))
-  (is (= 6 (choose-best-space minimax-board-2 "O"))))
+  (testing "it chooses the winning move if possible"
+    (is (= 4 (choose-best-space minimax-board-1 "O"))))
+  (testing "it blocks the opponent from winning if possible"
+    (is (= 6 (choose-best-space minimax-board-2 "O")))))
