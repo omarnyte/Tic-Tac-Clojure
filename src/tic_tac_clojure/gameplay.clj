74f9e53981a17c26ee07b745d06443595a3ce7a3
Omar De Los Santos <omarnyte@gmail.com>
2018-06-19 14:19:25 -0700

    Refactor #start-tic-tac-toe with #handle-end-of-game

--- src/tic_tac_clojure/gameplay.clj
+++ src/tic_tac_clojure/gameplay.clj
@@ -19,13 +19,20 @@
               (create-proto-player (:marker player-1) (:type player-1))
               (create-proto-player (:marker player-2) (:type player-2)))))
 
+(declare start-tic-tac-toe)
+            
+(defn- handle-end-of-game
+  []
+  (print-to-cli play-again-prompt)
+  (let [num-for-no 0 num-for-yes 1]
+  (case (get-valid-num-input num-for-no num-for-yes)
+    0 (print-to-cli thank-you-message)
+    1 (start-tic-tac-toe))))
+            
 (defn start-tic-tac-toe
   []
   (print-to-cli game-selection-prompt)
   (let [min (apply min (keys players))
         max (apply max (keys players))]
     (begin-selected-game (get-valid-num-input min max))
-    (print-to-cli play-again-prompt)
-    (case (get-valid-num-input 0 1)
-      0 (print-to-cli thank-you-message)
-      1 (recur))))
+    (handle-end-of-game)))
